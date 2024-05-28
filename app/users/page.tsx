8625b2db358b83915cf83d17168a291838fba97a
Mauro Schneider <103091+muschneider@users.noreply.github.com>
2024-05-28 14:01:31 -0300

    users api fix

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,12 +6,12 @@ import { getServerSession } from "next-auth";
 import options from "../api/auth/[...nextauth]/options";
 
 const Users = async () => {
-  // const session = await getServerSession(options);
-  //
-  // if (session?.user.role !== "ADMIN") {
-  //   return <p className="text-destructive">Admin access required !!! </p>;
-  // }
-  //
+  const session = await getServerSession(options);
+
+  if (session?.user.role !== "ADMIN") {
+    return <p className="text-destructive">Admin access required !!! </p>;
+  }
+
   const users = await prisma?.user.findMany();
   return (
     <div>
